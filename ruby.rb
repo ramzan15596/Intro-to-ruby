9ac306faafe39349e2428bead0ffdf578e6bbb95
Muhammad Ramzan <ramzan11596@gmail.com>
2018-04-02 21:43:02 +0500

    case statment practice assignment

--- ruby.rb
+++ ruby.rb
@@ -1,1 +1,17 @@
-puts "Hello World"
+bolean	=	"false"
+val2	=	"nil"
+
+case
+	when bolean == "pink elephant"
+		puts "Dont think about the pink elephant"
+
+	when val2.nil?
+		puts "Question mark in the method name"
+
+	when bolean == false
+		puts "Looks like this one should execute"
+
+	else
+		puts "I guess nothing matched... but why ?"
+
+end
