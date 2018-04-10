7a24b7fcfa7831966c340a861f35fa5663e1f5e5
Muhammad Ramzan <ramzan11596@gmail.com>
2018-04-10 21:21:37 +0500

    oop classes

--- ruby.rb
+++ ruby.rb
@@ -1,14 +1,45 @@
-family_tree_19 = {oldest: "jim", older: "joe", younger: "jack"}
-family_tree_19[:youngest]  = "jeremy"
-puts family_tree_19
+class Person
+	attr_reader :age	# getter only
+	attr_accessor :name  #setter and getter
+	# def initialize (name, age)
+	# 	@name = name
+	# 	@age = age
+	# end
+	def initialize (name, ageVar)
+		@name = name
+		self.age = ageVar #call the age= method
+		puts age
+	end
 
-def adjust_colors (props = {foreground: "red", background: "white"})
-	puts  "Foreground: #{props[:foreground]}" if props[:foreground]
-	puts "Background: #{props[:background]}" if props[:background]
+	def age= (new_age)
+		@age = new_age unless new_age > 120
+	end
+
+	#old getinfo function
+	# def get_info
+	# 	@additional_info = "Interesting"
+	# 	"Name #{@name}, age #{@age}"
+	# end
+
+
+	# def name
+	# 	@name
+	# end
+	# def name= (new_name)   #getter and setter method
+	# 	@name = new_name
+	# end
+
+	#attr_accessor :name, :age  #getter and setter easy method
 end
 
-adjust_colors
-
-adjust_colors ({ :foreground => "Green"})
-adjust_colors background: "yella"
-adjust_colors :background => "magenta"
+person1 = Person.new("Kim",14)
+puts "my age is #{person1.age}"
+person1.age = 130
+puts person1.age 
+# puts person1.name
+# puts person1.age
+# person1.name = "Mike"
+# puts person1.name
+# p person1.instance_variables
+# puts person1.get_info
+# p person1.instance_variables
