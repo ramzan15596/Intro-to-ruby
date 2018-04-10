a1d1a44cfb997e17d7ea3471efc509312d247298
Muhammad Ramzan <ramzan11596@gmail.com>
2018-04-10 22:10:17 +0500

    class ingeritance & methods & ||

--- ruby.rb
+++ ruby.rb
@@ -1,45 +1,63 @@
-class Person
-	attr_reader :age	# getter only
-	attr_accessor :name  #setter and getter
-	# def initialize (name, age)
-	# 	@name = name
-	# 	@age = age
-	# end
-	def initialize (name, ageVar)
-		@name = name
-		self.age = ageVar #call the age= method
-		puts age
+
+#|| operator
+# class Person
+# 	attr_reader :age	# getter only
+# 	attr_accessor :name  #setter and getter
+
+# 	def initialize (name, age)
+# 		@name = name
+# 		self.age = age #call the age= method
+# 	end
+
+# 	def age= (new_age)
+# 		@age ||= 5 #default for fist time
+# 		@age = new_age unless new_age > 120
+# 	end
+
+# end
+
+# person1 = Person.new("Kim",130)
+# puts person1.age
+# person1.age = 10
+# puts person1.age
+# person1.age = 200
+# puts person1.age
+
+# class method and variable
+# class Mathfunctions
+# 	def self.double(var)   # 1. using self
+# 		times_called; var * 2;
+# 	end
+# 	class << self  # 2. Using << self
+# 		def times_called
+# 			@@times_called ||=0; @@times_called +=1;
+# 		end
+# 	end
+# end
+# def Mathfunctions.triple(var)
+# 	times_called; var * 3;  #outside of a class
+# end
+# puts Mathfunctions.double 5
+# puts Mathfunctions.triple(3)
+# puts Mathfunctions.times_called
+
+
+
+class Dog  # Implicitly inherits from objects
+	def to_s 
+		"Dog"
 	end
-
-	def age= (new_age)
-		@age = new_age unless new_age > 120
+	def bark
+		"Barks loudly"
 	end
-
-	#old getinfo function
-	# def get_info
-	# 	@additional_info = "Interesting"
-	# 	"Name #{@name}, age #{@age}"
-	# end
-
-
-	# def name
-	# 	@name
-	# end
-	# def name= (new_name)   #getter and setter method
-	# 	@name = new_name
-	# end
-
-	#attr_accessor :name, :age  #getter and setter easy method
+end
+class SmallDog < Dog # < Denots Inherites
+	def bark #over ride
+		"Barks Quitly"
+	end
 end
 
-person1 = Person.new("Kim",14)
-puts "my age is #{person1.age}"
-person1.age = 130
-puts person1.age 
-# puts person1.name
-# puts person1.age
-# person1.name = "Mike"
-# puts person1.name
-# p person1.instance_variables
-# puts person1.get_info
-# p person1.instance_variables
+dog = Dog.new  #btw, new is a class method
+small_dog = SmallDog.new
+puts "#{dog}1 #{dog.bark}"
+puts "#{small_dog}2 #{small_dog.bark}"
