bd16f4a460921ea1868e4a6935eff2ac3a546036
Muhammad Ramzan <ramzan11596@gmail.com>
2018-04-11 22:31:10 +0500

    Module

--- ruby.rb
+++ ruby.rb
@@ -1,63 +1,40 @@
-
-#|| operator
-# class Person
-# 	attr_reader :age	# getter only
-# 	attr_accessor :name  #setter and getter
-
-# 	def initialize (name, age)
-# 		@name = name
-# 		self.age = age #call the age= method
-# 	end
-
-# 	def age= (new_age)
-# 		@age ||= 5 #default for fist time
-# 		@age = new_age unless new_age > 120
-# 	end
-
-# end
-
-# person1 = Person.new("Kim",130)
-# puts person1.age
-# person1.age = 10
-# puts person1.age
-# person1.age = 200
-# puts person1.age
-
-# class method and variable
-# class Mathfunctions
-# 	def self.double(var)   # 1. using self
-# 		times_called; var * 2;
-# 	end
-# 	class << self  # 2. Using << self
-# 		def times_called
-# 			@@times_called ||=0; @@times_called +=1;
-# 		end
+# module Sports
+# 	class Match
+# 		attr_accessor :score
 # 	end
 # end
-# def Mathfunctions.triple(var)
-# 	times_called; var * 3;  #outside of a class
+
+# module Patterns
+# 	class Match
+# 		attr_accessor :complete
+# 	end
 # end
-# puts Mathfunctions.double 5
-# puts Mathfunctions.triple(3)
-# puts Mathfunctions.times_called
+
+# match1 = Sports::Match.new
+# match1.score = 45; puts match1.score
+
+# match2 = Patterns::Match.new
+# match2.complete = true; puts match2.complete
 
 
-
-class Dog  # Implicitly inherits from objects
-	def to_s 
-		"Dog"
-	end
-	def bark
-		"Barks loudly"
-	end
-end
-class SmallDog < Dog # < Denots Inherites
-	def bark #over ride
-		"Barks Quitly"
+#module as mixin
+module SayMyName
+	attr_accessor :name
+	def print_name
+		puts "Name: #{@name}"
 	end
 end
 
-dog = Dog.new  #btw, new is a class method
-small_dog = SmallDog.new
-puts "#{dog}1 #{dog.bark}"
-puts "#{small_dog}2 #{small_dog.bark}"
+class Person
+	include SayMyName
+end
+class Company
+	include SayMyName
+end
+
+person = Person.new
+person.name = "Joe"
+person.print_name
+company = Company.new
+company.name = "Google & Microsoft LLC"
+company.print_name
